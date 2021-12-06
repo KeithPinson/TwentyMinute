7b0aa2e22d4d5437d53cb3ee498b5f071ecc7f03
Keith <me@keithpinson.com>
2021-12-06 09:38:57 -0500

    ✨ feat: Add script to copy icons per CSV

--- assets/icon_scripts/icons_copy_to_apps.py
+++ assets/icon_scripts/icons_copy_to_apps.py
@@ -3,6 +3,14 @@
 #
 # Copyright (c) 2021 Keith Pinson
 
+# Can be called with something like:
+#
+#    python icons_copy_to_apps.py
+#        --base ..\..
+#        --originals ..\icon_originals
+#        --generated ..\icons_generated
+#        --csv app_icon_checklist.csv
+
 import os
 import sys
 import argparse
@@ -40,14 +48,20 @@ def read_csv(csv_file):
     return fields, rows
 
 
-def generate_icon(fields, row, originals_path, generated_path, copy_to_apps):
+def generate_icon(fields, row, originals_path, generated_path, copy_to_apps, base):
+
+    project_base = os.path.abspath(base)
 
     icon_size_index = fields.get("Icon Size")
     from_size_index = fields.get("From Size")
     original_index = fields.get("Original")
+    extension_index = fields.get("Extension")
+    copy_name_index = fields.get("Copy Name")
 
     icon_size = row[icon_size_index] if icon_size_index is not None else ""
     from_size = row[from_size_index] if from_size_index is not None else ""
+    extension = row[extension_index] if extension_index is not None else "png"
+    copy_name = row[copy_name_index] if copy_name_index is not None else None
 
     original = True if original_index and row[original_index] == "1" else False
 
@@ -56,8 +70,10 @@ def generate_icon(fields, row, originals_path, generated_path, copy_to_apps):
     else:
         copy_original = False
 
-    original_file_path = os.path.join(originals_path, from_size + '.png')
-    generated_file_path = os.path.join(generated_path, icon_size + '.png')
+    original_file_path = os.path.join(
+        originals_path, from_size + '.' + extension)
+    generated_file_path = os.path.join(
+        generated_path, icon_size + '.' + extension)
 
     if not os.path.isfile(original_file_path):
         print("Original icon file not found:", original_file_path)
@@ -74,9 +90,30 @@ def generate_icon(fields, row, originals_path, generated_path, copy_to_apps):
             icon_image_file = from_image_file.resize(size)
             icon_image_file.save(generated_file_path)
 
+    if copy_to_apps and os.path.isfile(generated_file_path) and copy_name:
+        (file_path, file_name) = os.path.split(copy_name)
 
-def validate_csv(fields, row, originals_path, generated_path):
+        file_path = os.path.normpath(file_path)
+        file_path = os.path.join(project_base, file_path)
+
+        copy_to_path = os.path.join(file_path, file_name)
+
+        if not os.path.exists(file_path):
+            print("Path not found, making it:", file_path)
+            os.makedirs(file_path)
+
+        shutil.copyfile(generated_file_path, copy_to_path)
+
+
+def validate_csv(fields, row, originals_path, generated_path, base):
     valid = True
+
+    project_base = os.path.abspath(base)
+
+    if not os.path.exists(project_base):
+        valid = False
+        print("Project root not found:", project_base)
+        return valid
 
     if fields.get("Icon Size") is None:
         valid = False
@@ -112,6 +149,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        '--base', help='path of the project root', type=str, required=True)
+    parser.add_argument(
         '--originals', help='path of source icons', type=str, required=True)
     parser.add_argument(
         '--generated', help='path of generated icons', type=str, required=True)
@@ -127,7 +166,7 @@ if __name__ == '__main__':
     # Convert fields to a dictionary to make things easier
     fields = {k: v for v, k in enumerate(fields_list)}
 
-    if validate_csv(fields, rows, args.originals, args.generated):
+    if validate_csv(fields, rows, args.originals, args.generated, args.base):
         for row in rows:
             generate_icon(fields, row, args.originals,
-                          args.generated, args.copy)
+                          args.generated, args.copy, args.base)
