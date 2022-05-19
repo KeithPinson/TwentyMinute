e78290be5f62a419b23b7ba7dbc6fe49f25a53a4
Keith <me@keithpinson.com>
2022-05-19 12:03:06 -0400

    ⚗️wip: format tallyMarks string

--- lib/resources/tally_marks_db_query.dart
+++ lib/resources/tally_marks_db_query.dart
@@ -7,14 +7,36 @@
 /// @see [[LICENSE]] file in the root directory of this source.
 ///
 
+import 'package:jiffy/jiffy.dart';
+
 import 'package:twentyminute/main.dart';
 import 'package:twentyminute/resources/task_db_model.dart';
 
-Future<int> getTallyMarks(int startTime, int endTime) async {
+Future<List<Map<String, Object?>>> getTallyMarkList(int startTime, int endTime) async {
   var list = (await taskProvider.db!.query('Tasks',
       columns: ['_id', 'isDeleted', 'label', 'description', 'status', 'endTime'],
       where: 'isDeleted = 0 AND status=${taskStatus.done.index} '
-          'AND endTime >= $startTime AND endTime < $endTime'));
+          'AND endTime >= $startTime '
+          'AND endTime < $endTime'));
+
+  return list;
+}
+
+Future<int> getTallyMarks(Jiffy startTime, Jiffy endTime) async {
+  var list = await getTallyMarkList(
+      startTime.dateTime.millisecondsSinceEpoch,
+      endTime.dateTime.millisecondsSinceEpoch);
 
   return list.length;
 }
+
+Future<int> getTallyMarksToday() async {
+
+  var midnightThisMorning = Jiffy().startOf(Units.DAY);
+  var midnightTonight = Jiffy().add(days: 1).startOf(Units.DAY);
+
+  var count = getTallyMarks(midnightThisMorning, midnightTonight);
+
+  return count;
+}
+
--- lib/ui/tally_marks.dart
+++ lib/ui/tally_marks.dart
@@ -13,36 +13,68 @@ import 'package:twentyminute/components/tally_marks_cubit.dart';
 import 'package:twentyminute/components/timer_bloc.dart';
 
 import '../components/task_controller.dart';
+import '../resources/tally_marks_db_query.dart';
 
 class TallyMarks extends StatelessWidget {
   const TallyMarks({Key? key}) : super(key: key);
 
   String  getTallyMarks(int? count) {
-    var marks = ' '; // '$count';
+    var marks = ' ';
+
+    if (count != null && count > 0) {
+      var rem20 = count % 20;
+      var div20 = count ~/ 20;
+
+      var rem5 = rem20 % 5;
+      var div5 = rem20 ~/ 5;
+
+      // 5AFK
+      var chars =
+        ['1','2','3','4','5', '6','7','8','9','A',
+         'B','C','D','E','F', 'G','H','I','J','K'];
+
+      for (var t = div20; t > 0; t--) {
+        marks = marks + chars[5-1] + chars[10-1] + chars[15-1] + chars[20-1];
+      }
+
+      for (var f = 1; f <= div5; f++) {
+        marks = marks + chars[(5*f)-1];
+      }
+
+      marks = marks + chars[(div5*5)+rem5-1];
+    }
 
     return marks;
   }
 
-  //     context.read<TallyMarksCubit>().change();
-
   @override
   Widget build(BuildContext context) {
 
-    return BlocConsumer<TimerBloc,TimerState>(
-      listener: (context,state) {
+    int marks = 0;
+
+    return BlocConsumer<TimerBloc,TimerState> (
+      listener: (context,state) async {
         if (state == const TimerRunComplete()) {
-          // print( "BlocConsumer: TimerRunComplete" );
+          // marks = await getTallyMarksToday();
+          marks = 0;
         }
       },
       builder: (context,state) {
-        // return const Text( "13" );
         if (state == const TimerRunComplete() ) {
-          // create: (_) => TallyMarksCubit(0);
+
+          var tallyMarks = getTallyMarks(marks);
+          var l = tallyMarks.length;
+
+          // Clip the tallymarks to fit on one line
+          if (l > 9) {
+            tallyMarks = tallyMarks.substring(l - 9, l);
+          }
+
           return Padding(
             padding: EdgeInsets.symmetric(vertical: 3.0),
             child: Center(
               child: Text(
-                getTallyMarks(1),
+                tallyMarks,
                 textAlign: TextAlign.center,
                 overflow: TextOverflow.ellipsis,
                 style: const TextStyle(
