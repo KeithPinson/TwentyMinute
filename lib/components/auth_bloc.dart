ac0e270a5f76ef4e564162a22f402a75e46393fb
Keith <me@keithpinson.com>
2022-10-13 20:37:21 -0400

    🎉 init: Authentication BLOC

---
+++ lib/components/auth_bloc.dart
@@ -0,0 +1,67 @@
+/// Authentication BLOC
+///
+/// Copyright (c) Keith Pinson.
+///
+/// @see [[LICENSE]] file in the root directory of this source.
+///
+
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_firebase_login/repository/user_repository.dart';
+import 'package:meta/meta.dart';
+
+part './auth_bloc_event.dart';
+part './auth_bloc_state.dart';
+
+
+
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+
+  final UserRepository _userRepository;
+
+  AuthenticationBloc({ @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository;
+
+
+
+  @override
+  AuthenticationState get initialState => Uninitialized();
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+      AuthenticationEvent event,
+      ) async* {
+    if (event is AppStarted) {
+      yield* _mapAppStartedToState();
+    } else if (event is LoggedIn) {
+      yield* _mapLoggedInToState();
+    } else if (event is LoggedOut) {
+      yield* _mapLoggedOutToState();
+    }
+  }
+
+  Stream<AuthenticationState> _mapAppStartedToState() async* {
+    try {
+      final isSignedIn = await _userRepository.isSignedIn();
+      if (isSignedIn) {
+        final name = await _userRepository.getUsername();
+        yield Authenticated(name);
+      } else {
+        yield Unauthenticated();
+      }
+    } catch(_) {
+      yield Unauthenticated();
+    }
+  }
+
+  Stream<AuthenticationState> _mapLoggedInToState() async* {
+    final name = await _userRepository.getUsername();
+    yield Authenticated(name);
+  }
+
+  Stream<AuthenticationState> _mapLoggedOutToState() async* {
+    yield Unauthenticated();
+    _userRepository.signOut();
+  }
+}
---
+++ lib/components/auth_bloc_event.dart
@@ -0,0 +1,8 @@
+/// Authentication BLOC event
+///
+/// Copyright (c) Keith Pinson.
+///
+/// @see [[LICENSE]] file in the root directory of this source.
+///
+
+part of 'auth_bloc.dart';
